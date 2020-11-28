1e81e4a8c7c45ca85eb146dbcca328922eddf063
Pedram Valiani <p.valiani@gmail.com>
2020-11-28 21:12:56 +0000

    tested merchant has type/tag

--- tests/merchant_type_test.py
+++ tests/merchant_type_test.py
@@ -4,4 +4,7 @@ from models.merchant_type import MerchantType
 class TestMerchantType(unittest.TestCase):
     
     def setUp(self):
-        pass
+        self.merchant_type = MerchantType("Entertainment")
+
+    def test_merchant_has_a_type(self):
+        self.assertEqual("Entertainment", self.merchant_type.name)
