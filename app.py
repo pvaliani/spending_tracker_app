a3b9d7b224baa44802dea16f25c427fe70423701
Pedram Valiani <p.valiani@gmail.com>
2020-11-28 22:35:30 +0000

    updated app.py with blueprints for views

--- app.py
+++ app.py
@@ -2,12 +2,21 @@ from flask import Flask, render_template
 
 # CONTROLLER BLUEPRINT IMPORTS HERE
 
+from controllers.transactions_controller import transactions_blueprint
+from controllers.amounts_controller import amounts_blueprint
+from controllers.merchants_controller import merchants_blueprint
+from controllers.merchant_types_controller import merchant_types_blueprint
+
 
 app = Flask(__name__)
 
 
 # BLUEPRINT REGISTRATION HERE 
 
+app.register_blueprint(transactions_blueprint)
+app.register_blueprint(amounts_blueprint)
+app.register_blueprint(merchants_blueprint)
+app.register_blueprint(merchant_types_blueprint)
 
 
 @app.route("/")
