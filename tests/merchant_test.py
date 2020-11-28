36ff5c1393a383ba260227525d10c95aca12c33e
Pedram Valiani <p.valiani@gmail.com>
2020-11-28 21:10:32 +0000

    tested merchant has name and type

--- tests/merchant_test.py
+++ tests/merchant_test.py
@@ -5,4 +5,11 @@ from models.merchant_type import MerchantType
 class TestMerchant(unittest.TestCase):
     
     def setUp(self):
-        pass
+        self.merchant = Merchant("Amazon", "General Purpose")
+
+    def test_merchant_has_a_name(self):
+        self.assertEqual("Amazon", self.merchant.name)
+    
+    def test_merchant_has_a_tag_category(self):
+        self.assertEqual("General Purpose", self.merchant.merchant_type)
+    
