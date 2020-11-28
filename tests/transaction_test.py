dab57dc42c050fda07bb5becfc650ae47187411a
Pedram Valiani <p.valiani@gmail.com>
2020-11-28 21:15:08 +0000

    basic testing of all methods working

--- tests/transaction_test.py
+++ tests/transaction_test.py
@@ -8,4 +8,10 @@ from models.transaction import Transaction
 class TestTransaction(unittest.TestCase):
     
     def setUp(self):
-        pass
+        self.transaction = Transaction(50, "eBay")
+
+    def test_transaction_has_an_amount(self):
+        self.assertEqual(50, self.transaction.amount)
+
+    def test_transaction_has_a_merchant(self):
+        self.assertEqual("eBay", self.transaction.merchant)
