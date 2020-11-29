1901ab836a4b7f49a57f3a4156655a84653b3540
Pedram Valiani <p.valiani@gmail.com>
2020-11-29 19:48:27 +0000

    adds amount_repository sql queries

--- repositories/amount_repository.py
+++ repositories/amount_repository.py
@@ -1,12 +1,24 @@
 from db.run_sql import run_sql
 from models.amount import Amount
 
+# - Save a new amount value into amounts table
+# - SQL statement to input into table
+# - Set the value attribute of amount to a list 
+# - run_sql takes a list and returns a list of dictionaries
+# - The id of the amount is then the very first position of the results list of dict and it's dict key is id, dict value is the id associated with ['id']
+
 def save(amount):
     sql = "INSERT INTO amounts (value) VALUES (%s) RETURNING id"
     values = [amount.value]
     results = run_sql(sql, values)
     id = results[0]['id']
     amount.id = id
+
+# - SQL: Select all table data from amounts
+# - Run the SQL runner method
+# - Loop through the results
+# - Results is a list of dict. So we define an amount as being the key/value pair at each index of the results list of dict
+# - Append each amount into the amounts list and return it so we have all selected items in the database
 
 
 def select_all():
@@ -18,6 +30,8 @@ def select_all():
         amounts.append(amount)
     return amounts
 
+# - SQL = Select all table entries from amounts when presented with an id
+# - id has no attributes but is index position 0 in the list of values
 
 def select(id):
     sql = "SELECT * FROM amounts WHERE id = %s"
@@ -26,17 +40,21 @@ def select(id):
     amount = Amount(result["value"], result["id"])
     return amount
 
+# - SQL: delete all from the table. Execute SQL runner
 
 def delete_all():
     sql = "DELETE FROM amounts"
     run_sql(sql)
 
+# - SQL: delete the amount from the table when given an id
 
 def delete(id):
     sql = "DELETE FROM amounts WHERE id = %s"
     values = [id]
     run_sql(sql, values)
 
+# - SQL: update the amounts tables user spend value where the id is ""
+# - run_sql expects a list and returns a list of dict. Therefore provide an amount value and amount id to identify the update required.
 
 def update(amount):
     sql = "UPDATE amounts SET value = %s WHERE id = %s"
