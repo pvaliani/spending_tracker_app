716b256ee6c3e159f2f0d58de3e4b73b07687d5e
Pedram Valiani <p.valiani@gmail.com>
2020-11-30 17:10:12 +0000

    adds user_budget_repository to replace previous method of storing a users budget

---
+++ repositories/user_budget_repository.py
@@ -0,0 +1,39 @@
+from db.run_sql import run_sql
+from models.transaction import Transaction
+from models.amount import Amount
+from models.user_budget import UserBudget
+from models.merchant import Merchant
+
+import repositories.amount_repository as amount_repository
+import repositories.merchant_repository as merchant_repository
+
+
+def save(user_budget):
+    sql = "INSERT INTO user_budgets (value) VALUES (%s)"
+    values = [user_budget.value]
+    results = run_sql(sql, values)
+    value = results[0]['value']
+    user_budget.value = value
+
+
+def select_all():
+    user_budgets = []
+    sql = "SELECT * FROM user_budgets"
+    results = run_sql(sql)
+    for result in results:
+        user_budget = UserBudget(result["value"])
+        user_budgets.append(user_budget)
+    return user_budgets
+
+# - SQL: delete all from the table. Execute SQL runner
+
+def delete_all():
+    sql = "DELETE FROM user_budgets"
+    run_sql(sql)
+
+
+def update(user_budget):
+    sql = "UPDATE user_budgets SET value = %s WHERE value = %s"
+    values = [user_budget.value]
+    run_sql(sql, values)
+
