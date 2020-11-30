bf58c9b997ce82d865ad9986c2ee57a850433eb2
Pedram Valiani <p.valiani@gmail.com>
2020-11-30 00:04:30 +0000

    more user_budget controller fixes

--- controllers/user_budgets_controller.py
+++ controllers/user_budgets_controller.py
@@ -21,28 +21,28 @@ def new_user_budget():
 @user_budgets_blueprint.route("/userbudgets", methods=["POST"])
 def create_merchant_type():
     value = request.form["value"]
-    new_user_budget = MerchantType(value)
+    new_user_budget = UserBudget(value)
     merchant_type_repository.save(new_user_budget)
     return redirect("/userbudgets")
 
 
 # EDIT
-@user_budgets_blueprint.route("/userbudgets/<id>/edit")
-def edit_merchant_type(id):
-    merchant_type = merchant_type_repository.select(id)
-    return render_template('user_budgets/edit.html', merchant_type=merchant_type)
+@user_budgets_blueprint.route("/userbudgets/edit")
+def edit_merchant_type():
+    user_budget = merchant_type_repository.select()
+    return render_template('user_budgets/edit.html', user_budget=user_budget)
 
 
 # UPDATE
-@user_budgets_blueprint.route("/userbudgets/<id>", methods=["POST"])
-def update_merchant(id):
+@user_budgets_blueprint.route("/userbudgets/<>", methods=["POST"])
+def update_merchant():
     value = request.form["value"]
-    merchant_type = MerchantType(value, id)
-    merchant_type_repository.update(merchant_type)
+    user_budget = UserBudget(value)
+    merchant_type_repository.update(user_budget)
 
 
 # DELETE
-@user_budgets_blueprint.route("/userbudgets/<id>/delete", methods=["POST"])
-def delete_merchant(id):
-    merchant_type_repository.delete(id)
+@user_budgets_blueprint.route("/userbudgets/delete", methods=["POST"])
+def delete_merchant():
+    merchant_type_repository.delete()
     return redirect("/userbudgets")
