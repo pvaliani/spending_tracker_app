16b41139ada6e90b5249d90d8773092059d4ac1c
Pedram Valiani <p.valiani@gmail.com>
2020-12-01 21:48:08 +0000

    still updating css and added about page

--- app.py
+++ app.py
@@ -25,6 +25,10 @@ app.register_blueprint(user_budgets_blueprint)
 def main():
     return render_template('index.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run()
 
