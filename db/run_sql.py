88bf6cfd1eeb77f85f854d3272c30c6ddf56d713
Pedram Valiani <p.valiani@gmail.com>
2021-01-24 19:00:39 +0000

    production branch heroku deployment

--- db/run_sql.py
+++ db/run_sql.py
@@ -2,13 +2,28 @@
 # - Need to create runner for heroku db
 
 import psycopg2
-import psycopg2.extras
+import psycopg2.extras 
+import urllib.parse as urlparse
+import os
+
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+dbname = url.path[1:]
+user = url.username
+password = url.password
+host = url.hostname
+port = url.port
 
 def run_sql(sql, values = None):
     results = []
     conn = None
     try:
-        conn = psycopg2.connect("dbname='transactions'")
+        conn = psycopg2.connect(
+        dbname=dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port
+        )
         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         cur.execute(sql, values)
         conn.commit()
